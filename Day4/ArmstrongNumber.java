f94b9397be6372a84015ea94a798fc68dac2d78b
H A R S H  ! <145217050+Harshbhagat22@users.noreply.github.com>
2024-11-22 17:09:53 +0530

    Add files via upload

---
+++ Day4/ArmstrongNumber.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public static boolean isArmstrong(int n){
+        int temp = n;
+        int sum = 0;
+        int digits = Integer.toString(n).length();
+        while(n>0){
+            int last = n%10;
+            sum += Math.pow(last,digits);
+            n = n/10;
+        }
+        
+        
+        return temp == sum;
+    }
+}
+
+public class ArmstrongNumber
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.print(Solution.isArmstrong(n));
+	}
+}
